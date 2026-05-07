7f9d2bb1e2f30c60ab6426725c74d72e878c4f40
antonsalkutan-pixel <antonsalkutan@gmail.com>
2026-05-07 07:56:59 +0300

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,45 @@
+body {
+    font-family: Arial;
+    width: 700px;
+    margin: 30px auto;
+    background: #f4f4f4;
+}
+
+h1 {
+    text-align: center;
+}
+
+form {
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+input,
+textarea,
+select {
+    width: 100%;
+    padding: 10px;
+    margin-top: 5px;
+    margin-bottom: 15px;
+}
+
+button {
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    background: white;
+}
+
+table, th, td {
+    border: 1px solid black;
+}
+
+th, td {
+    padding: 10px;
+    text-align: left;
+}
